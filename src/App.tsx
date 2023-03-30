307efc2c149dc0569b80d5e85c5aa66bcb5f3716
郭立华 <guoliehua@gmail.com>
2023-03-30 23:50:37 +0800

    fix: 集成Redux到项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { Button } from 'antd'
-import { CiCircleFilled } from '@ant-design/icons'
+import { useAppDispath, useAppSelector } from './hooks/reduxHooks'
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from './store/reducers/counter'
 import './App.css'
 
 function App() {
+  const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispath()
+  
   return (
-    <div className='font-bold text-red-500 bg-[#ccc] h-screen w-screen'>
-      <Button type="primary" className='flex items-center'>
-        <CiCircleFilled size={16}/> Hello world!
+    <div className="w-[200px] flex flex-wrap mx-auto">
+      <h2 className="text-center text-lg font-bold text-indigo-600">{count}</h2>
+
+      <Button onClick={() => dispatch(increment())}>InCrement</Button>
+      <Button onClick={() => dispatch(decrement())}>DeCrement</Button>
+
+      <Button onClick={() => dispatch(incrementByAmount(6))}>
+        InCrement + 6
       </Button>
     </div>
   )
