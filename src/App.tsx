61cb4372e6c8f3615190e7991a9139dbbee446be
郭立华 <guo.lihua@ruanyun.net>
2023-03-30 18:13:48 +0800

    feat: 集成tailwindcss和antd到项目中

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from 'antd'
+import { CiCircleFilled } from '@ant-design/icons'
 import './App.css'
 
 function App() {
   return (
-    <div >
-      Hello world!
+    <div className='font-bold text-red-500 bg-[#ccc] h-screen w-screen'>
+      <Button type="primary" className='flex items-center'>
+        <CiCircleFilled size={16}/> Hello world!
+      </Button>
     </div>
   )
 }
